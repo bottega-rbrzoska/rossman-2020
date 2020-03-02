072b0d57825c69c94b9bb77b933e491956c0f81d
Rafal Brzoska <rbrzoska@future-processing.com>
2020-03-02 13:19:40 +0100

    initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyTestComponent } from './my-test/my-test.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductItemComponent } from './products/product-item/product-item.component';
+import { ProductAddComponent } from './products/product-add/product-add.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
+import { ProductEditComponent } from './products/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyTestComponent
+    MyTestComponent,
+    HomeComponent,
+    ContactComponent,
+    NavigationComponent,
+    PageNotFoundComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductAddComponent,
+    ProductFormComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
