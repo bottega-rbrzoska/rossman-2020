3b2b817afde89282b5ac7e9c55c921b617fc76de
Rafal Brzoska <rbrzoska@future-processing.com>
2020-03-02 13:09:50 +0100

    bootstrap

---
+++ src/app/my-test/my-test.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'ros-my-test',
+  templateUrl: './my-test.component.html',
+  styleUrls: ['./my-test.component.scss'],
+
+})
+export class MyTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
